9d28fbeb57ab8eb147ce36dc101dc9f1c4ca834c
John Jansen <johnjan@microsoft.com>
2019-10-11 15:02:46 -0700

    Add project files.

---
+++ ADOIterationSummary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+
+namespace ADOIterationSummary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const String c_collectionUri = "https://dev.azure.com/microsoft";
+            const String c_projectName = "Edge";
+            const String c_repoName = "chromium.src";
+
+            // Interactively ask the user for credentials, caching them so the user isn't constantly prompted
+            VssCredentials creds = new VssClientCredentials();
+            creds.Storage = new VssClientCredentialStorage();
+
+            // Connect to Azure DevOps Services
+            VssConnection connection = new VssConnection(new Uri(c_collectionUri), creds);
+
+            // Get a GitHttpClient to talk to the Git endpoints
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+            // Get data about a specific repository
+            var repo = gitClient.GetRepositoryAsync(c_projectName, c_repoName).Result;
+
+            // Create instance of WorkItemTrackingHttpClient using VssConnection
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            // Get 2 levels of query hierarchy items
+            List<QueryHierarchyItem> queryHierarchyItems = witClient.GetQueriesAsync(c_projectName, depth: 2).Result;
+
+            // Search for 'My Queries' folder
+            QueryHierarchyItem myQueriesFolder = queryHierarchyItems.FirstOrDefault(qhi => qhi.Name.Equals("My Queries"));
+            if (myQueriesFolder != null)
+            {
+                var foo = "Skippy";
+            }
+
+
+
+        }
+    }
+}
